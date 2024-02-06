50815d0643f606a6a3d80aca4f05285a98b6ea97
xcastillo25 <xcastillo25@gmail.com>
2024-02-06 13:25:34 -0600

    Servidor subido

--- planificacion/src/config/config.js
+++ planificacion/src/config/config.js
@@ -1,2 +1,6 @@
-export const API_URL = 'http://localhost:8020/api';
-export const API_SERVER = 'http://localhost:8020'
+// export const API_URL = 'http://localhost:8020/api';
+// export const API_SERVER = 'http://localhost:8020'
+
+//planificacion-back-5xm6nmeia-alexis-castillos-projects.vercel.app
+
+export const API_URL = 'https://backend-planificacion.onrender.com/api';
